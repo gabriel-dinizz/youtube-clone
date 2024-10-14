37413e052c75e695fe719b2cbd6fbd31ccb6c88b
gabriel-dinizz <dinizcremelgabriel@gmail.com>
2024-10-14 08:43:26 -0300

    local directories

--- video-processing-service/src/storage.ts
+++ video-processing-service/src/storage.ts
@@ -5,3 +5,16 @@ import fs from 'fs';
 import ffmpeg from 'fluent-ffmpeg';
 
 const storage = new Storage();
+
+const rawVideoBucketName =  'youtube-clone-raw-videos';
+const processedVideoBucketName = 'youtube-clone-processed-videos';
+
+const localFilePath = 'video.mp4';
+const localFilePathProcessed = 'video_processed.mp4';
+
+// Creates local directories for the raw and processed videos
+
+export function createLocalDirectories() {
+    ensureDirectoryExistence(localFilePath);
+    ensureDirectoryExistence(localFilePathProcessed);
+}
