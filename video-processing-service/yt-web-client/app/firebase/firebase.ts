4a46430c2b6b7026e3fac6711cdb5b9bfd2eae9a
gabriel-dinizz <dinizcremelgabriel@gmail.com>
2024-10-28 11:18:00 -0300

    Refactor firebase.ts: Add firebase configuration

--- video-processing-service/yt-web-client/app/firebase/firebase.ts
+++ video-processing-service/yt-web-client/app/firebase/firebase.ts
@@ -1,2 +1,9 @@
 import { intializeApp } from 'firebase/app';    // import the initializeApp function from the firebase/app module
 import { getAuth } from 'firebase/auth';       // import the getAuth function from the firebase/auth module     
+
+const firebaseConfig = { 
+    apiKey: "your apiKey",
+    authDomain: "<your authDomain",  
+    projectId: "<your projectId>",  
+    appId: "<your appId>",
+};
