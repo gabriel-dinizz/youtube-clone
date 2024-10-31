01e2d9764d166158112923fc0bee976f29e61335
gabriel-dinizz <dinizcremelgabriel@gmail.com>
2024-10-31 20:22:47 -0300

    commit

--- video-processing-service/yt-web-client/app/navbar/sign-in.tsx
+++ video-processing-service/yt-web-client/app/navbar/sign-in.tsx
@@ -1,7 +1,12 @@
 import { signInWithGoogle, signOut } from "../firebase/firebase";
 import styles from "./navbar.module.css";   
 import { User } from "../firebase/firebase";    
+import { Sign } from "crypto";
 
 interface SignInProps {
     user: User | null;
+}
+
+export default function SignIn( { user}: SignInProps) {
+    
 }
